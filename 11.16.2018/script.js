fcdbe8effec88cfe83418300b55878ab3f99c00f
elvinseferov <44227537+elvinseferov@users.noreply.github.com>
2018-11-19 02:01:30 -0800

    Add files via upload

---
+++ 11.16.2018/script.js
@@ -0,0 +1,20 @@
+var a = document.querySelector("#tab1");
+a.addEventListener("click",function(){
+    console.log("test");
+})
+var a = document.querySelector("#tab2");
+a.addEventListener("click",function(){
+    console.log("test");
+})
+var a = document.querySelector("#tab3");
+a.addEventListener("click",function(){
+    console.log("test");
+})
+var a = document.querySelector("#tab4");
+a.addEventListener("click",function(){
+    console.log("test");
+})
+var a = document.querySelector("#tab5");
+a.addEventListener("click",function(){
+    console.log("test");
+})
